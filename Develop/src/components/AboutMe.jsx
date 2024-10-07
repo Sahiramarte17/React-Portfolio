452d9cecaae62c8c6ab9b24b0bd4c4b5b744efc8
Sahiramarte17 <sahira@sahiras-mbp.myfiosgateway.com>
2024-10-07 13:24:33 -0400

    Aboutme.jsx

---
+++ Develop/src/components/AboutMe.jsx
@@ -0,0 +1,14 @@
+// src/components/AboutMe.jsx
+import React from 'react';
+
+const AboutMe = () => {
+  return (
+    <section id="about">
+      <h2>About Me</h2>
+      <img src="path-to-your-photo.jpg" alt="Developer" />
+      <p>Short bio about yourself.</p>
+    </section>
+  );
+};
+
+export default AboutMe;
