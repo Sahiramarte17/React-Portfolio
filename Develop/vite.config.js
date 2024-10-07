fee81da9862fc3ee793b269b18b0f364a92923f8
Sahiramarte17 <sahira@Sahiras-MacBook-Pro.local>
2024-10-07 14:12:36 -0400

    vite.config.js

--- Develop/vite.config.js
+++ Develop/vite.config.js
@@ -1,7 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -9,3 +5,4 @@ export default defineConfig({
     open: true
   }
 })
+
