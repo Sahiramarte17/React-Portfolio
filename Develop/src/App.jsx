2b92b3d65455627bb706b2da70d1869385c681ab
Sahiramarte17 <sahira@sahiras-mbp.myfiosgateway.com>
2024-10-07 13:01:13 -0400

    App.jsx

--- Develop/src/App.jsx
+++ Develop/src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css';
-import BucketList from './components/Navigation';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
+
