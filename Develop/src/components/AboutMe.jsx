1c0994d47ea188fe5816618fabdc662205dbc799
Sahiramarte17 <sahira@sahiras-mbp.myfiosgateway.com>
2024-10-07 22:22:25 -0400

    updating colors

--- Develop/src/components/AboutMe.jsx
+++ Develop/src/components/AboutMe.jsx
@@ -9,7 +9,12 @@ const AboutMe = () => {
       <br></br>
       <img src="../assets/myimage.jpg" alt="Developer" />
       <br></br>
-      <p>     Sahira Marte is an experienced staffing coordinator with 12 years of expertise in workforce management and a master's degree in business administration. Her career has been marked by strong leadership, strategic decision-making, and an ability to optimize staffing solutions that align with business goals. Passionate about software development, Sahira is dedicated to advancing her technical skills and exploring innovative ways to merge her business acumen with technology. She views software development as a creative avenue to solve problems and bring ideas to life, positioning herself at the intersection of business and tech.</p>
+      <p>     Sahira Marte is an experienced staffing coordinator with 12 years of expertise in workforce 
+        management and a master's degree in business administration. Her career has been marked by strong 
+        leadership, strategic decision-making, and an ability to optimize staffing solutions that align with 
+        business goals. Passionate about software development, Sahira is dedicated to advancing her technical 
+        skills and exploring innovative ways to merge her business acumen with technology. She views software 
+        development as a creative avenue to solve problems and bring ideas to life, positioning herself at the intersection of business and tech.</p>
     </section>
   );
 };
--- Develop/src/components/Resume.jsx
+++ Develop/src/components/Resume.jsx
@@ -5,13 +5,29 @@ const Resume = () => {
   return (
     <section id="resume">
       <h2>Resume</h2>
-      <a href="path-to-your-resume.pdf" download>Download Resume</a>
-      <h3>Proficiencies</h3>
+      <br></br>
+      <a href="path-to-your-resume.pdf" download>Download Resume</a> 
+      <br></br>
+     
+ <h3>Front-end Proficiencies</h3>
       <ul>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>CSS</li>
-        {/* Add more proficiencies */}
+        <li> * JavaScript</li>
+        <li> * React</li>
+        <li> * CSS</li>
+        <li> * JQuery</li>
+        <li> * React</li>
+        <li> * Bootstrap</li>
+        </ul>
+       <br></br>
+       <h4>Back-end Proficiencies</h4>
+      <ul>
+        <li> * APIs</li>
+        <li> * Node</li>
+        <li> * Express</li>
+        <li> * MySQL</li>
+        <li> * Python</li>
+        <li> * JavaScript</li>
+
       </ul>
     </section>
   );
